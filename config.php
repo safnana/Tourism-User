b0b48fafd7010c7ff24e9c59a672367146d77528
saaaa <139216836+safnana@users.noreply.github.com>
2024-06-27 07:54:23 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";//error karena ada spasi
+$password = "";
 $dbname = "database_jj";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
