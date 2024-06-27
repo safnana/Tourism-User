d2e3753728451bcb66dddbb8f5379776b5b3bf7a
saaaa <139216836+safnana@users.noreply.github.com>
2024-06-27 07:54:10 +0700

    Update ceksession.php

--- ceksession.php
+++ ceksession.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (!isset($_SESSION['id_user'])) { //error seharusnya id_user bukan idUser
+if (!isset($_SESSION['id_user'])) { 
     $error_message = "Anda harus login terlebih dahulu";
     echo "<script>alert('$error_message'); window.location.href='home.php';</script>";
     exit();
